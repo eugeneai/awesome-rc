1599c1323d8320245a6c2df01bea0175f68da40f
Evgeny Cherkashin <eugene@irnok.net>
2016-11-06 09:43:55 +0800

    Update from awesome distro config.

--- merge-root.sh
+++ merge-root.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 RC=rc.lua
 OUT=/etc/xdg/awesome/$RC
+git pull origin master
+git mergetool --tool=meld
 git checkout main
 cp -f $OUT ./
-git commit -m "Update." -a
+git commit -m "Update from awesome distro config." -a
 git checkout master
-git merge main -m "Merge with upstream."
+git merge main -m "Merge with awesome distro config."
 git mergetool --tool=meld
-
-
+git rm -f *.orig
+git commit -a
+git push origin master
