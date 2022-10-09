a601ce99f43bc6d30f238f0eba1665cd9776fe89
Evgeny Cherkashin <eugeneai@irnok.net>
2022-10-09 16:33:44 +0800

    Returned lain centerwork layout.

--- rc.lua
+++ rc.lua
@@ -126,6 +126,7 @@ local lain = require("lain")
 -- Table of layouts to cover with awful.layout.inc, order matters.
 tag.connect_signal("request::default_layouts", function()
     awful.layout.append_default_layouts({
+        lain.layout.centerwork,
         awful.layout.suit.floating,
         lain.layout.termfair,
         -- awful.layout.suit.max,
